d4f1a4a0ab77a15716283eb885cf1c7271fce85d
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-09-01 20:16:02 +0530

    fix bugs

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ await connectDB();
 
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
+server.listen(PORT, "0.0.0.0",()=> console.log("Server is running on PORT: " + PORT));
 
 
 // Export server for Vervel
